d0c45c432fe7740f5d513a271f767fd99a5c43a6
Fangming Lan <starlitlight@users.noreply.github.com>
2024-07-14 16:06:25 +0200

    improve code readability

--- Program.cs
+++ Program.cs
@@ -5,16 +5,13 @@ namespace MyApplication
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        public static void Main(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+                .ConfigureWebHostDefaults(webBuilder => 
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                })
+                .Build()
+                .Run();
     }
 }
