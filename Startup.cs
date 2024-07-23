7b824d6b2f8f8073d12a49015b816809dcd83a51
Fangming Lan <starlitlight@users.noreply.github.com>
2024-07-23 19:21:28 +0200

    added function

--- Startup.cs
+++ Startup.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Mvc.Versioning;
 
 public class Startup
 {
@@ -15,6 +17,25 @@ public class Startup
 
     public void ConfigureServices(IServiceCollection services)
     {
+        services.AddControllers();
+
+        // Add API Versioning to the project
+        services.AddApiVersioning(options =>
+        {
+            // Specify the default API Version as 1.0
+            options.DefaultApiVersion = new ApiVersion(1, 0);
+            // If the client does not specify the API version in the request, use the default API version.
+            options.AssumeDefaultVersionWhenUnspecified = true;
+            // Advertise the API versions supported for the particular endpoint
+            options.ReportApiVersions = true;
+            // Add support for multiple versioning scheme (e.g., by query string, header, etc.)
+            options.ApiVersionReader = ApiVersionReader.Combine(
+                new QueryStringApiVersionReader("api-version"),
+                new HeaderApiVersionReader("X-Version"),
+                new MediaTypeApiVersionReader("version"));
+        });
+
+        services.AddRouting(options => options.LowercaseUrls = true);
     }
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
