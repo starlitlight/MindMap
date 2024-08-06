432b4949cdee97d2b291bdcc094caaaeb8531965
Fangming Lan <starlitlight@users.noreply.github.com>
2024-08-06 12:16:15 +0200

    add error catching

--- Controllers/MapsController.cs
+++ Controllers/MapsController.cs
@@ -1,8 +1,8 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using YourAppNamespace.Models;
-using System;
 
 namespace YourAppNamespace.Controllers
 {
@@ -15,54 +15,94 @@ namespace YourAppNamespace.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<MindMap>> GetAll()
         {
-            return Ok(_mindMaps);
+            try
+            {
+                return Ok(_mindMaps);
+            }
+            catch (Exception ex)
+            {
+                // Log the exception details here using your logging framework
+                return StatusCode(500, "An internal error occurred while fetching all MindMaps.");
+            }
         }
 
         [HttpGet("{id}")]
         public ActionResult<MindMap> Get(int id)
         {
-            var mindMap = _mindMaps.FirstOrDefault(x => x.Id == id);
-            if (mindMap == null)
+            try
             {
-                return NotFound();
+                var mindMap = _mindMaps.FirstOrDefault(x => x.Id == id);
+                if (mindMap == null)
+                {
+                    return NotFound();
+                }
+                return Ok(mindMap);
             }
-            return Ok(mindMap);
+            catch (Exception ex)
+            {
+                // Log the exception details here using your logging framework
+                return StatusCode(500, "An internal error occurred while retrieving specific MindMap.");
+            }
         }
 
         [HttpPost]
         public ActionResult<MindMap> Create(MindMap mindMap)
         {
-            mindMap.Id = _mindMaps.Any() ? _mindMaps.Max(x => x.Id) + 1 : 1;
-            _mindMaps.Add(mindMap);
-            return CreatedAtAction(nameof(Get), new { id = mindMap.Id }, mindMap);
+            try
+            {
+                mindMap.Id = _mindMaps.Any() ? _mindMaps.Max(x => x.Id) + 1 : 1;
+                _mindMaps.Add(mindMap);
+                return CreatedAtAction(nameof(Get), new { id = mindMap.Id }, mindMap);
+            }
+            catch (Exception ex)
+            {
+                // Log the exception details here using your logging framework
+                return StatusCode(500, "An error occurred while creating the MindMap.");
+            }
         }
 
         [HttpPut("{id}")]
         public ActionResult Update(int id, MindMap mindMap)
         {
-            var existingMindMap = _mindMaps.FirstOrDefault(x => x.Id == id);
-            if (existingMindMap == null)
+            try
             {
-                return NotFound();
+                var existingMindMap = _mindMaps.FirstOrDefault(x => x.Id == id);
+                if(existingMindMap == null)
+                {
+                    return NotFound();
+                }
+                
+                existingMindMap.Name = mindMap.Name;
+                existingMindMap.Content = mindMap.Content;
+
+                return NoContent();
             }
-
-            existingMindMap.Name = mindMap.Name;
-            existingMindMap.Content = mindMap.Content;
-
-            return NoContent();
+            catch (Exception ex)
+            {
+                // Log the exception details here using your logging framework
+                return StatusCode(500, "An error occurred while updating the MindMap.");
+            }
         }
 
         [HttpDelete("{id}")]
         public ActionResult Delete(int id)
         {
-            var mindMap = _mindMaps.FirstOrDefault(x => x.Id == id);
-            if (mindMap == null)
+            try
             {
-                return NotFound();
-            }
+                var mindMap = _mindMaps.FirstOrDefault(x => x.Id == id);
+                if (mindMap == null)
+                {
+                    return NotFound();
+                }
 
-            _mindMaps.Remove(mindMap);
-            return NoContent();
+                _mindMaps.Remove(mindMap);
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                // Log the exception details here using your logging framework
+                return StatusCode(500, "An error occurred while deleting the MindMap.");
+            }
         }
     }
 }
